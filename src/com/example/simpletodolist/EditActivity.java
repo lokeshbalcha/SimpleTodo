727f2f24373bce078fdac5504a8909d288376dad
Venkatasai Lokesh Balcha <lokeshb@dutyought-lm.corp.yahoo.com>
2014-06-05 16:58:40 -0700

    Sampletodo app completed

--- src/com/example/simpletodolist/EditActivity.java
+++ src/com/example/simpletodolist/EditActivity.java
@@ -1,21 +1,56 @@
 package com.example.simpletodolist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class EditActivity extends Activity {
+	
+	EditText etEditItem;
+	int pos;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit);
 		
-		EditText etEditItem = (EditText)findViewById(R.id.etEditItem);
-		etEditItem.setText(getIntent().getStringExtra("toedit"));	
+		etEditItem = (EditText)findViewById(R.id.etEditItem);
+		pos = getIntent().getIntExtra("pos", -1);
+		etEditItem.setText(getIntent().getStringExtra("toedit"));
+		etEditItem.setSelection(getIntent().getStringExtra("toedit").length());
 		
+		
+	
+	 final Button button = (Button) findViewById(R.id.save);
+     button.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View v) {
+             // Perform action on click   
+        	 
+        	 EditText etsave = (EditText) findViewById(R.id.etEditItem);
+        	 
+             Intent sendback = new Intent(EditActivity.this, TodoActivity.class);
+             EditedItem(etsave);
+             //EditActivity.this.startActivity(sendback);
+         }
+     });
 	}
-	
-	
-	
+
+
+
+    public void EditedItem(View v) {
+    	Intent i = new Intent();
+    	String itemText = etEditItem.getText().toString();
+    	i.putExtra("edited", itemText);
+		i.putExtra("position", pos);
+    	setResult(RESULT_OK, i);
+    	finish();
+    }
+
 }
+
--- src/com/example/simpletodolist/TodoActivity.java
+++ src/com/example/simpletodolist/TodoActivity.java
@@ -9,6 +9,8 @@ import org.apache.commons.io.FileUtils;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -18,6 +20,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 public class TodoActivity extends Activity {
+	private static final int REQUEST_CODE = 0;
 	ArrayList<String> items;
 	ArrayAdapter<String> itemsAdapter;
 	ListView lvItems;
@@ -31,8 +34,8 @@ public class TodoActivity extends Activity {
         readItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("Item one");
-        items.add("Item two");
+        //items.add("Item one");
+        //items.add("Item two");
         setupListViewListener();
     }
 
@@ -54,8 +57,10 @@ public class TodoActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view, int position,
 					long id) {
 				Intent intent = new Intent(TodoActivity.this, EditActivity.class);
+				intent.putExtra("pos", position);
+				Log.d("asdw", position+"");
 				intent.putExtra("toedit", items.get(position).toString());
-		        TodoActivity.this.startActivity(intent);
+		        TodoActivity.this.startActivityForResult(intent, 0);
 				}
 		});
 
@@ -64,8 +69,9 @@ public class TodoActivity extends Activity {
 	public void addTodoItem(View v){
 		EditText etNewItem = (EditText)findViewById(R.id.etNewItem);
 		itemsAdapter.add(etNewItem.getText().toString());
-		etNewItem.setText("");		
+		etNewItem.setText("");
 		saveItems();
+		
 	}
 	
 	private void saveItems() {
@@ -89,5 +95,21 @@ public class TodoActivity extends Activity {
 			e.printStackTrace();
 		}
 	}
-		
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  // REQUEST_CODE is defined above
+		//Log.d("asdw", "here");
+	  if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+		  
+	     String editItemText = data.getExtras().getString("edited");
+	     int position = data.getExtras().getInt("position");
+	     Log.d("asdw", position+"");
+	     items.set(position, editItemText);
+		 itemsAdapter.notifyDataSetChanged();
+		 saveItems();
+	  }
+	} 
+	
+	
 }
